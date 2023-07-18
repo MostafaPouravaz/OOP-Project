c397b13c3c556b06b8f36544851b953ea50d9f5e
USER <mo.javad.bijani@gmail.com>
2023-07-18 14:22:00 +0330

    salam

--- oop/Main.java
+++ oop/Main.java
@@ -18,4 +18,5 @@ public class Main {
         new File("oop\\\\files\\\\restaurantRatings.json");
         RegisterMenu registerMenu = RegisterMenu.getInstance();
         registerMenu.run();
-    }}
+    }
+}
