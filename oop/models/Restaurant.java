e0802ccae0e1507aa3c13557652ae69be85e2809
USER <mo.javad.bijani@gmail.com>
2023-07-19 01:29:11 +0330

    salam

--- oop/models/Restaurant.java
+++ oop/models/Restaurant.java
@@ -105,25 +105,36 @@ public class Restaurant {
     }
 
     public ArrayList<Food> getFoods() {
-        if (loadRestaurantFromFile() != null)
-            allRestaurant = new ArrayList<>(loadRestaurantFromFile());
+
+//        if (loadRestaurantFromFile() != null)
+//            allRestaurant = new ArrayList<>(loadRestaurantFromFile());
 //        if (foods.size() == 0)
 //            foods = new ArrayList<>();
-//        foods.addAll(Food.getAllRestaurantFoods(getRestaurantID()));
+//        for (int i=0; i<Food.getAllFoods().size(); i++){
+//            if (Food.getAllFoods().get(i).getID_restaurant() == getRestaurantID()) {
+//                if (foods.size() == 0)
+//                    foods.add(Food.getAllFoods().get(i));
+//                for (int j = 0; j < foods.size(); j++) {
+//                    if (!foods.contains(Food.getAllFoods().get(i)));
+//                }
+//            }
+//        }
+//        return foods;
+
+        foods = Food.foodsSort(this.RestaurantID);
+
         return Food.getAllRestaurantFoods(getRestaurantID());
     }
 
     public ArrayList<RatingForRestaurant> getAllRatings() {
         if (loadRestaurantFromFile() != null)
             allRestaurant = new ArrayList<>(loadRestaurantFromFile());
-        allRatings = RatingForRestaurant.getAllRatingsByRID(getRestaurantID());
         return allRatings;
     }
 
     public ArrayList<CommentForRestaurant> getAllComments() {
         if (loadRestaurantFromFile() != null)
             allRestaurant = new ArrayList<>(loadRestaurantFromFile());
-        allComments = CommentForRestaurant.getAllCommentsByRID(getRestaurantID());
         return allComments;
     }
 
@@ -252,7 +263,7 @@ public class Restaurant {
             allRestaurant = new ArrayList<>();
             if (allR != null)
                 allRestaurant.addAll(allR);
-            ID_Counter = allRestaurant.size()+1;
+            ID_Counter = allRestaurant.size();
         } catch (IOException e) {
             System.out.println(" ");
         }
